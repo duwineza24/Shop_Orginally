2da944b8b10c0b8a32d53d8c57ffe71cc5a4b4d7
Devothe Uwineza <devothauwineza7@gmail.com>
2026-02-06 14:50:33 +0200

    update backend

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/Shop_Orginally/', // Exact match with capital S and underscore
-});
